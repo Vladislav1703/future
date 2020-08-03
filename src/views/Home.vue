b1da4dc9e717ddec76572293e13b2719bf810681
Vladislav <lokotlt@gmail.com>
2020-08-03 16:55:20 +0300

    as

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -42,6 +42,7 @@ export default {
   },
   data() {
     return {
+      actualRow: {},
       list: '',
       BigList:
         'http://www.filltext.com/?rows=1000&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&delay=3&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D',
@@ -80,7 +81,7 @@ export default {
       return this.$store.dispatch('SORT_LIST', { key: sortBy, sortArg: 1 });
     },
     showAllInfo(row) {
-      console.log(row);
+      this.actualRow = row;
     },
   },
 };
